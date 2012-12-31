5f6009b2d1bc4942aa9b4d059432c0da5f8e7b5a
maximilianmueller <maximilian.mueller@web.de>
2012-12-31 08:19:13 +0100

    anpassungen am admin gui: - log-datensätze nur filtern, kein zeigen in der detailansicht mehr (unnötig)

--- admin/admin_log.php
+++ admin/admin_log.php
@@ -56,7 +56,7 @@ $opts['inc'] = 20;
 // Options you wish to give the users
 // A - add,  C - change, P - copy, V - view, D - delete,
 // F - filter, I - initial sort suppressed
-$opts['options'] = 'VF';
+$opts['options'] = 'F';
 
 // Number of lines to display on multiple selection filters
 $opts['multiple'] = '4';
