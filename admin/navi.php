a6eef29c0e914c503ad659c373dc40a3c3e81149
Wolfgang Buecker <wolfgang.buecker@arcor.de>
2013-01-03 22:20:20 +0100

    Datei export_calc.php hinzugefuegt

--- admin/navi.php
+++ admin/navi.php
@@ -2,4 +2,6 @@
 <a href="index.php">Start</a> |
 <a href="admin_ma.php">Mitarbeiter</a> |
 <a href="admin_dump.php">Zeiterfassung Ausgabe</a> |
-<a href="admin_log.php">Zeiterfassung Log</a>
+<a href="admin_log.php">Zeiterfassung Log</a> |
+<a href="export_calc.php">Datenexport nach Calc</a> 
+
