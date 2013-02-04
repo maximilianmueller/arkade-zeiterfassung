b104b829852e8ac4188ca02c79e9e1d82e01c131
maximilianmueller <maximilian.mueller@web.de>
2013-02-04 10:45:25 +0100

    backup-skripte: username, passwort und datenbank-name in pfad-konfigurationsdatei "ze_backup_paths.bat" ausgelagert

--- backup/ze_full_backup.bat
+++ backup/ze_full_backup.bat
@@ -1,7 +1,7 @@
-@echo off
+rem @echo off
 call ze_backup_paths.bat
 set fileName=ze_full_backup_%date:~6,4%_%date:~3,2%_%date:~0,2%.sql 
-%mySQLBinPath%\mysqldump -u wolfgang -palanish5 --database zeiterfassung > %localPath%\full_backup\%fileName%
+%mySQLBinPath%\mysqldump -u %db_user% -p%db_pass% --database %db_name% > %localPath%\full_backup\%fileName%
 xcopy %localPath%\full_backup\%fileName% %backupPath%\full_backup /y /c /q
 
 
