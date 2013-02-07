b7a2c4519bbf465395ed1ed819a49bde8277e2e6
maximilianmueller <maximilian.mueller@web.de>
2013-02-07 22:46:34 +0100

    logout-link auf admin-seiten

--- admin/navi.php
+++ admin/navi.php
@@ -4,5 +4,15 @@
 <a href="admin_dump.php">Zeiterfassung Ausgabe</a> |
 <a href="admin_log.php">Zeiterfassung Log</a> |
 <a href="admin_uks.php">Urlaub Krank Schule</a> |
-<a href="admin_export.php">Datenexport nach Calc</a> 
+<a href="admin_export.php">Datenexport nach Calc</a> |
+<a href="javascript:doLogout()">Ausloggen</a>
 
+<script type="text/javascript">
+
+function doLogout ()
+  {
+  alert ("Sie werden jetzt ausgeloggt. Bitte anschlieﬂend das Fenster schlieﬂen.")
+  window.location.href = "//logout@" + window.location.host + window.location.pathname;
+  }
+
+</script>
